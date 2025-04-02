b448621cf07fa08c904f15b97376d9416bf77050
SpaceMovie1992 <anahusic8@gmail.com>
2025-04-02 21:50:14 +0200

    fixed templates iter and print now everything works as expected

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -3,15 +3,14 @@
 #include <iostream>
 #include <cstddef>
 
-template <typename T, typename F>
-void iter(T* array, size_t length, F func)
-{
-	for (size_t i = 0; i < length; i++)
-		func(array[i]);
+template <typename T>
+void iter(T *array, size_t len, void (*func)(T &)) {
+
+    for (size_t i = 0; i < len; i++)
+        func(array[i]);
 };
 
 template <typename T>
-void print(const T& x)
-{
-	std::cout << x << " ";
+void print(T &a) {
+    std::cout << a << std::endl;
 };
