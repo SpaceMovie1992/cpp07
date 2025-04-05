d27e8c0a44a64420293852d2fce8cd87887b9230
SpaceMovie1992 <anahusic8@gmail.com>
2025-04-05 20:17:27 +0200

    finished the main function modified the header file, main function provides tests for every case

--- ex02/inc/Array.hpp
+++ ex02/inc/Array.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
 
 template <typename T>
 class Array
@@ -17,81 +18,81 @@ public:
 	unsigned int size() const;
 
 private:
-	T *_array;
+	T *_arr;
 	unsigned int _size;
 };
 
 template <typename T>
 Array<T>::Array()
 {
-    this->_arr = nullptr;
-    this->_size = 0;
+	this->_arr = nullptr;
+	this->_size = 0;
 }
 
 template <typename T>
 Array<T>::Array(unsigned int n)
 {
-    this->_arr = new T[n]();
-    this->_size = n;
+	this->_arr = new T[n]();
+	this->_size = n;
 }
 
 template <typename T>
 Array<T>::Array(const Array &src)
 {
-    this->_size = src._size;
-    this->_arr = new T[this->_size];
+	this->_size = src._size;
+	this->_arr = new T[this->_size];
 
-    for (unsigned int i = 0; i < this->_size; ++i)
-    {
-        this->_arr[i] = src._arr[i];
-    }
+	for (unsigned int i = 0; i < this->_size; ++i)
+	{
+		this->_arr[i] = src._arr[i];
+	}
 }
 
 template <typename T>
 Array<T> &Array<T>::operator=(const Array &rhs)
 {
-    if (this == &rhs)
-        return *this;
+	if (this == &rhs)
+		return *this;
 
-    delete[] this->_arr;
-    this->_size = rhs._size;
-    this->_arr = new T[this->_size];
+	delete[] this->_arr;
+	this->_size = rhs._size;
+	this->_arr = new T[this->_size];
 
-    for (unsigned int index = 0; index < this->_size; ++index)
-    {
-        this->_arr[index] = rhs._arr[index];
-    }
+	for (unsigned int index = 0; index < this->_size; ++index)
+	{
+		this->_arr[index] = rhs._arr[index];
+	}
 
-    return *this;
+	return *this;
 }
 
 template <typename T>
 Array<T>::~Array()
 {
-    if (this->_arr)
-        delete[] this->_arr;
+	if (this->_arr)
+		delete[] this->_arr;
 }
 
 template <typename T>
 T &Array<T>::operator[](unsigned int index)
 {
-    if (index >= this->_size)
-        throw std::out_of_range("Accessing out-of-bounds index in Array");
-    
-    return this->_arr[index];
+	if (index >= this->_size)
+		throw std::out_of_range("Accessing out-of-bounds index in Array");
+
+	return this->_arr[index];
 }
 
 template <typename T>
 const T &Array<T>::operator[](unsigned int index) const
 {
-    if (index >= this->_size)
-        throw std::out_of_range("Accessing out-of-bounds index in Array (const)");
-    
-    return this->_arr[index];
+	if (index >= this->_size)
+		throw std::out_of_range("Accessing out-of-bounds index in Array (const)");
+
+	return this->_arr[index];
 }
 
 template <typename T>
 unsigned int Array<T>::size() const
 {
-    return this->_size;
+	return this->_size;
 }
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,2 +1,57 @@
 #include "Array.hpp"
 
+int main()
+{
+	std::cout << "== Empty Array Test ==" << std::endl;
+	Array<int> empty;
+	std::cout << "Size: " << empty.size() << std::endl;
+
+	try
+	{
+		empty[0] = 42;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "\n== Int Array Initialization ==" << std::endl;
+	Array<int> numbers(5);
+	for (unsigned int i = 0; i < numbers.size(); ++i)
+		numbers[i] = i * 10;
+
+	for (unsigned int i = 0; i < numbers.size(); ++i)
+		std::cout << "numbers[" << i << "] = " << numbers[i] << std::endl;
+
+	std::cout << "\n== Copy Constructor Test ==" << std::endl;
+	Array<int> copy(numbers);
+	numbers[0] = 999;
+	std::cout << "copy[0] = " << copy[0] << " (should not be 999)" << std::endl;
+
+	std::cout << "\n== Assignment Operator Test ==" << std::endl;
+	Array<int> assigned;
+	assigned = numbers;
+	numbers[1] = 888;
+	std::cout << "assigned[1] = " << assigned[1] << " (should not be 888)" << std::endl;
+
+	std::cout << "\n== Out of Bounds Access ==" << std::endl;
+	try
+	{
+		std::cout << numbers[42] << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "\n== Array of Strings ==" << std::endl;
+	Array<std::string> words(3);
+	words[0] = "hello";
+	words[1] = "array";
+	words[2] = "template";
+
+	for (unsigned int i = 0; i < words.size(); ++i)
+		std::cout << "words[" << i << "] = " << words[i] << std::endl;
+
+	return 0;
+}
